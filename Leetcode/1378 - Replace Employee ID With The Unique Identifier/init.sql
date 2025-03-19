d629313cc17ee93f5ef7e213d1a60fe7acc500cf
Georg Graf (SPG) <grafg@spengergasse.at>
2025-03-19 08:18:55 +0100

    renames

---
+++ Leetcode/1378 - Replace Employee ID With The Unique Identifier/init.sql
@@ -0,0 +1,12 @@
+Employees TABLE: + ----+----------+
+| id | name | + ----+----------+
+| 1 | Alice | | 7 | Bob | | 11 | Meir | | 90 | Winston | | 3 | Jonathan | + ----+----------+
+EmployeeUNI TABLE: + ----+-----------+
+| id | unique_id | + ----+-----------+
+| 3 | 1 | | 11 | 2 | | 90 | 3 | + ----+-----------+
+/ / Employees TABLE: + ----+----------+
+| id | name | + ----+----------+
+| 1 | Alice | | 7 | Bob | | 11 | Meir | | 90 | Winston | | 3 | Jonathan | + ----+----------+
+EmployeeUNI TABLE: + ----+-----------+
+| id | unique_id | + ----+-----------+
+| 3 | 1 | | 11 | 2 | | 90 | 3 | + ----+-----------+
